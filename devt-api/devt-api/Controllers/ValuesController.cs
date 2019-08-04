46799c35f5e0f91f4505b5abbb0b9639549b9b8b
TimeReapz <souldarkfear@gmail.com>
2019-08-04 19:44:44 +0700

    cv2

--- devt-api/devt-api/Controllers/ValuesController.cs
+++ devt-api/devt-api/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace devt_api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value3" };
+            return new string[] { "value1", "value5" };
         }
 
         // GET api/values/5
